75a203d97dda613ddcac3472ec57f9c3d0df99f0
cohoh <cohohohn04@gmail.com>
2022-05-18 19:45:15 +0200

    Remove: .idea Directory

--- src/main/java/de/hobst/geotaggen/Main.java
+++ src/main/java/de/hobst/geotaggen/Main.java
@@ -2,37 +2,25 @@ package de.hobst.geotaggen;
 
 import com.google.maps.DirectionsApi;
 import com.google.maps.GeoApiContext;
-import com.google.maps.errors.ApiException;
 import com.google.maps.model.*;
 
-import java.io.IOException;
 import java.sql.*;
 import java.time.Instant;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException, InterruptedException, ApiException {
-        String DatabaseName = "Basti";
-        String TabelName = "route";
-        //MYSQL mysql = new MYSQL("138.201.202.3", "Basti", "4711!", 3306);
-//        GeoApiContext context = new GeoApiContext.Builder().apiKey("AIzaSyBWtdzqzuvBU4j6KBp5dKqh2-tR4lslSx4").build();
+    public static void main(String[] args)  {
         GeoApiContext context = new GeoApiContext.Builder().apiKey("AIzaSyBrqZbPz-RfAXPaZMSsfzhfhMKkhlsaxAI").build();
-
-
         try {
-            Connection con = DriverManager.getConnection(
-                    "jdbc:mysql://138.201.202.3:3306/Basti", "Basti", "4711!");
-
-
-            Statement stmt=con.createStatement();
-            ResultSet rs=stmt.executeQuery("select * from route");
-            while(rs.next()) {
-                //System.out.println(rs.getInt(1) + "  " + rs.getString(2) + "  " + rs.getString(3));
+            Connection con = DriverManager.getConnection("jdbc:mysql://138.201.202.3:3306/Basti", "Basti", "4711!");
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery("select * from route");
+            while (rs.next()) {
                 int plz = rs.getInt("plz");
                 int id = rs.getInt("ID");
                 String ort = rs.getString("ort");
                 System.out.println(id + " " + plz + " " + ort);
-                if(rs.getObject("Entfernung") != null){
+                if (rs.getObject("Entfernung") != null) {
                     System.out.println("Übersprungen");
                     continue;
                 }
@@ -48,7 +36,7 @@ public class Main {
                     for (DirectionsLeg leg : route.legs) {
                         System.out.println(leg.distance.humanReadable);
 
-                        Double entf = (double)leg.distance.inMeters/1000;
+                        Double entf = (double) leg.distance.inMeters / 1000;
                         double roundOff = Math.round(entf * 10.0) / 10.0;
                         System.out.println(entf);
                         System.out.println(roundOff);
@@ -60,23 +48,12 @@ public class Main {
                         preparedStmt.executeUpdate();
                     }
                 }
-
             }
-
-
             con.close();
-
         } catch (Exception e) {
             System.out.println(e);
         }
-
-
-
-
-
     }
-
-
 }
 
 
